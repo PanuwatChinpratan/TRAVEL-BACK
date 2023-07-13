23cb9dda90e44b2762c033c5fbdcd5a1ea8c37b6
PanuwatChinpratan <panuwatchinpratan@gmail.com>
2023-07-14 01:56:22 +0700

    s

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -1,37 +1,184 @@
+import User from "../models/User.js";
 import Booking from "../models/Booking.js";
-import { verifyToken } from "../utils/auth.js";
+import { verifyToken, verifyUser } from "../utils/verifyToken.js";
+
+export const createUser = async (req, res) => {
+  const newUser = new User(req.body);
+  try {
+    const savedUser = await newUser.save();
+    res.status(200).json({
+      success: true,
+      message: "User created successfully",
+      data: savedUser,
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: "Failed to create User",
+      error: err.message,
+    });
+  }
+};
+
+export const updateUser = async (req, res) => {
+  const id = req.params.id;
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json({
+      success: true,
+      message: "User updated successfully",
+      data: updatedUser,
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: "Failed to update User",
+      error: err.message,
+    });
+  }
+};
+
+export const deleteUser = async (req, res) => {
+  const id = req.params.id;
+  try {
+    const deletedUser = await User.findByIdAndDelete(id);
+    if (!deletedUser) {
+      return res.status(404).json({
+        success: false,
+        message: "User not found",
+      });
+    }
+    res.status(200).json({
+      success: true,
+      message: "User deleted successfully",
+      data: deletedUser,
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: "Failed to delete User",
+      error: err.message,
+    });
+  }
+};
+
+export const getSingleUser = async (req, res) => {
+  const id = req.params.id;
+  try {
+    const user = await User.findById(id);
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: "User not found",
+      });
+    }
+    res.status(200).json({
+      success: true,
+      message: "User retrieved successfully",
+      data: user,
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: "Failed to retrieve User",
+      error: err.message,
+    });
+  }
+};
+
+export const getAllUser = async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.status(200).json({
+      success: true,
+      message: "Users retrieved successfully",
+      data: users,
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: "Failed to retrieve Users",
+      error: err.message,
+    });
+  }
+};
 
 export const createBooking = async (req, res) => {
-  verifyToken(req, res, async () => {
-    const newBooking = new Booking(req.body);
-    try {
-      const savedBooking = await newBooking.save();
-      res.status(200).json({
-        success: true,
-        message: "Your tour is booked",
-        data: savedBooking,
-      });
-    } catch (err) {
-      res.status(500).json({ success: false, message: "Internal server error" });
-    }
+  verifyToken(req, res, () => {
+    verifyUser(req, res, async () => {
+      const newBooking = new Booking(req.body);
+      try {
+        const savedBooking = await newBooking.save();
+        res.status(200).json({
+          success: true,
+          message: "Your tour is booked",
+          data: savedBooking,
+        });
+      } catch (err) {
+        res.status(500).json({
+          success: false,
+          message: "Internal server error",
+          error: err.message,
+        });
+      }
+    });
   });
 };
 
 export const getBooking = async (req, res) => {
   const id = req.params.id;
   try {
-    const book = await Booking.findById(id);
-    res.status(200).json({ success: true, message: "Successful", data: book });
+    const booking = await Booking.findById(id);
+    if (!booking) {
+      return res.status(404).json({
+        success: false,
+        message: "Booking not found",
+      });
+    }
+    res.status(200).json({
+      success: true,
+      message: "Booking retrieved successfully",
+      data: booking,
+    });
   } catch (err) {
-    res.status(404).json({ success: false, message: "Not found" });
+    res.status(500).json({
+      success: false,
+      message: "Failed to retrieve Booking",
+      error: err.message,
+    });
   }
 };
 
 export const getAllBooking = async (req, res) => {
   try {
-    const books = await Booking.find();
-    res.status(200).json({ success: true, message: "Success", data: books });
+    const bookings = await Booking.find({});
+    res.status(200).json({
+      success: true,
+      message: "Bookings retrieved successfully",
+      data: bookings,
+    });
   } catch (err) {
-    res.status(500).json({ success: false, message: "Internal server error" });
+    res.status(500).json({
+      success: false,
+      message: "Failed to retrieve Bookings",
+      error: err.message,
+    });
   }
+};
+
+export default {
+  createUser,
+  updateUser,
+  deleteUser,
+  getSingleUser,
+  getAllUser,
+  createBooking,
+  getBooking,
+  getAllBooking,
 };
