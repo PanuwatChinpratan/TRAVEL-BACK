502a83f353bb5fe4aa04aadbc67e702494544a60
PanuwatChinpratan <panuwatchinpratan@gmail.com>
2023-07-14 01:38:19 +0700

    v

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -1,37 +1,37 @@
 import Booking from "../models/Booking.js";
+import { verifyToken } from "../utils/auth.js";
 
 export const createBooking = async (req, res) => {
-  const newBooking = new Booking(req.body);
-  try {
-    const savedBooking = await newBooking.save();
-    res
-      .status(200)
-      .json({
+  verifyToken(req, res, async () => {
+    const newBooking = new Booking(req.body);
+    try {
+      const savedBooking = await newBooking.save();
+      res.status(200).json({
         success: true,
-        message: "Yout your is booked",
+        message: "Your tour is booked",
         data: savedBooking,
       });
-  } catch (err) {
-    res.status(500).json({ success: false, message: "internal server error" });
-  }
+    } catch (err) {
+      res.status(500).json({ success: false, message: "Internal server error" });
+    }
+  });
 };
 
 export const getBooking = async (req, res) => {
   const id = req.params.id;
   try {
     const book = await Booking.findById(id);
-    res.status(200).json({ success: true, message: "successful", data: book });
+    res.status(200).json({ success: true, message: "Successful", data: book });
   } catch (err) {
-    res.status(404).json({ success: false, message: "not found" });
+    res.status(404).json({ success: false, message: "Not found" });
   }
 };
 
-
-export const getAllBooking = async (res) => {
+export const getAllBooking = async (req, res) => {
   try {
     const books = await Booking.find();
-    res.status(200).json({ success: true, message: "success", data: books });
+    res.status(200).json({ success: true, message: "Success", data: books });
   } catch (err) {
-    res.status(500).json({ success: false, message: "internal server error" });
+    res.status(500).json({ success: false, message: "Internal server error" });
   }
 };
