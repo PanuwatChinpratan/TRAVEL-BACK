ab046f26c39123365de7d3cb7b11e220d24ee951
PanuwatChinpratan <panuwatchinpratan@gmail.com>
2023-07-31 07:40:02 +0700

    g

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 8000;
 const corsOptions = {
-  origin: ["https://travel-front-henna.vercel.app"],
+  origin: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true,
 };
